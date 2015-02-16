83537fb5d9ba71cfd9822aff1cbde020ed62b3bd
Michael Lloyd <commander.bubble@gmail.com>
2015-02-16 14:41:21 +1100

    updates documentation, moves types in/out of md5 namespace

--- src/md5.h
+++ src/md5.h
@@ -34,26 +34,28 @@
 
 #include <string>
 
+/*
+ * Size of a standard MD5 signature in bytes.  This definition is for
+ * external programs only.  The MD5 routines themselves reference the
+ * signature as 4 unsigned 32-bit integers.
+ */
+const unsigned int MD5_SIZE = (4 * sizeof(unsigned int)); //16
+
+
  namespace md5 {
-
-    /*
-     * Size of a standard MD5 signature in bytes.  This definition is for
-     * external programs only.  The MD5 routines themselves reference the
-     * signature as 4 unsigned 32-bit integers.
-     */
-    #define MD5_SIZE	16
-
     /*
      * NOTE: the following is assumed to generate a 32-bit unsigned data
      * type.
      */
-    #define MAX_MD5_UINT32		((unsigned int)4294967295U)
+//    #define MAX_MD5_UINT32 ((unsigned int)4294967295U)
+    const unsigned int UINT32_MAX = 4294967295U;
 
     /*
      * The MD5 algorithm works on blocks of characters of 64 bytes.  This
      * is an internal value only and is not necessary for external use.
      */
-    #define MD5_BLOCK_SIZE		64
+//    #define MD5_BLOCK_SIZE 64
+    const unsigned int BLOCK_SIZE = 64;
 
     class md5_t {
         public:
@@ -65,8 +67,8 @@
              * Initialize structure containing state of MD5 computation. (RFC 1321,
              * 3.3: Step 3).  This is for progressive MD5 calculations only.  If
              * you have the complete string available, call it as below.
-             * md5_t::process should be called for each bunch of bytes and after the
-             * last process call, md5_t::finish should be called to get the signature.
+             * process should be called for each bunch of bytes and after the last
+             * process call, finish should be called to get the signature.
              *
              * RETURNS:
              *
@@ -85,7 +87,7 @@
              *
              * This function is used to calculate a MD5 signature for a buffer of
              * bytes.  If you only have part of a buffer that you want to process
-             * then md5_t::md5_t, md5_t::process, and md5_t::finish should be used.
+             * then md5_t, process, and finish should be used.
              *
              * RETURNS:
              *
@@ -106,7 +108,7 @@
              *
              * DESCRIPTION:
              *
-             * This function is used to progressively calculate a MD5 signature some
+             * This function is used to progressively calculate an MD5 signature some
              * number of bytes at a time.
              *
              * RETURNS:
@@ -139,10 +141,45 @@
              *
              * signature_ - A 16 byte buffer that will contain the MD5 signature.
              */
-            void finish(void* signature_);
+            void finish(void* signature_ = NULL);
 
-            void get_signature(void* signature_);
-            void get_string(void* str_);
+            /*
+             * get_sig
+             *
+             * DESCRIPTION:
+             *
+             * Retrieves the previously calculated signature from the MD5 object.
+             *
+             * RETURNS:
+             *
+             * None.
+             *
+             * ARGUMENTS:
+             *
+             * signature_ - A 16 byte buffer that will contain the MD5 signature.
+             */
+            void get_sig(void* signature_);
+
+            /*
+             * get_string
+             *
+             * DESCRIPTION:
+             *
+             * Retrieves the previously calculated signature from the MD5 object in
+             * printable format.
+             *
+             * RETURNS:
+             *
+             * None.
+             *
+             * ARGUMENTS:
+             *
+             * str_ - a string of characters which should be at least 33 bytes long
+             * (2 characters per MD5 byte and 1 for the \0).
+             *
+             * str_len - the length of the string.
+             */
+            void get_string(void* str_, const unsigned int str_len);
 
         private:
             /* internal functions */
@@ -150,23 +187,23 @@
             void process_block(const void*, const unsigned int);
             void get_result(void*);
 
-            unsigned int md_A;                          /* accumulater 1 */
-            unsigned int md_B;                          /* accumulater 2 */
-            unsigned int md_C;                          /* accumulater 3 */
-            unsigned int md_D;                          /* accumulater 4 */
+            unsigned int md_A;                  /* accumulator 1 */
+            unsigned int md_B;                  /* accumulator 2 */
+            unsigned int md_C;                  /* accumulator 3 */
+            unsigned int md_D;                  /* accumulator 4 */
 
-            unsigned int md_total[2];                   /* totaling storage */
-            unsigned int md_buf_len;                    /* length of the storage buffer */
-            char md_buffer[MD5_BLOCK_SIZE * 2];         /* character storage buffer */
+            unsigned int md_total[2];           /* totalling storage */
+            unsigned int md_buf_len;            /* length of the storage buffer */
+            char md_buffer[m5::BLOCK_SIZE * 2]; /* character storage buffer */
 
-            bool finished;                              /* object state */
+            bool finished;                      /* object state */
 
-            char signature[4 * sizeof(unsigned int)];   /* stored signature */
-            char str[33];                               /* stored plain text hash */
+            char signature[MD5_SIZE];           /* stored signature */
+            char str[33];                       /* stored plain text hash */
     };
 
     /*
-     * md5_sig_to_string
+     * sig_to_string
      *
      * DESCRIPTION:
      *
@@ -181,7 +218,7 @@
      *
      * signature - a 16 byte buffer that contains the MD5 signature.
      *
-     * str - a string of charactes which should be at least 33 bytes long (2
+     * str - a string of characters which should be at least 33 bytes long (2
      * characters per MD5 byte and 1 for the \0).
      *
      * str_len - the length of the string.
@@ -189,7 +226,7 @@
     extern void sig_to_string(const void* signature, char* str, const int str_len);
 
     /*
-     * md5_sig_from_string
+     * sig_from_string
      *
      * DESCRIPTION:
      *
@@ -208,7 +245,6 @@
      * characters per MD5 byte).
      */
     extern void sig_from_string(void* signature, const char* str);
-
-} // namespace
+} // namespace md5
 
 #endif /* ! __MD5_H__ */
